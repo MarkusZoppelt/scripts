949c9833113814b20e02f7a765b0314df050f0e4
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2017-06-27 13:19:10 +0200

    added createSolution.zsh for automatically creating .sln files from .c sources

---
+++ createSolution.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+filename=$1
+projectName=${filename:r}
+
+mkdir $projectName
+mkdir $projectName/$projectName
+
+cd $projectName
+
+cp ../$filename $projectName/.
+cp ~/code/Vanilla-sln/Vanilla.sln $projectName.sln
+cp ~/code/Vanilla-sln/Vanilla.vcxproj $projectName/$projectName.vcxproj
+
+rm -rf .vs* .DS_Store
+sed -i -- "s/Vanilla/$projectName/g" **/*(D.)
+rm -rf ../**/*.*--*
