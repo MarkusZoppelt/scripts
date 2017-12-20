4c00c944973a1a7be46cfa231ecc15ff105b254f
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2017-12-20 09:27:03 +0100

    new CAN setup

--- setupCantactInterface.sh
+++ setupCantactInterface.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo slcand -o -c -s6 /dev/ttyACM* can0 && echo "Interface can0 added..."
-sudo ifconfig can0 up && echo "can0 is up!"
-ifconfig can0
+sudo slcand -o -c -s6 /dev/ttyACM*
+sudo ip link set up slcan0 && echo "Interface is up!"
+ifconfig slcan0
