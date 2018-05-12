3f6eb3745f5a42bb2b74089fb587620de045344f
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2018-05-12 14:33:30 +0200

    added candump.py

---
+++ candump.py
@@ -0,0 +1,8 @@
+from pyvit import can
+from pyvit.hw.cantact import CantactDev
+
+dev = CantactDev("/dev/cu.usbmodem1451")
+dev.set_bitrate(500000)
+dev.start()
+while True:
+      print(dev.recv())
