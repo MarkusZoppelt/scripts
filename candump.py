0caeab0b35cc43d92cbda0448cb7c827bf7f22a6
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2018-06-25 07:10:06 +0200

    modified candump.py

--- candump.py
+++ candump.py
@@ -2,7 +2,7 @@
 from pyvit import can
 from pyvit.hw.cantact import CantactDev
 
-dev = CantactDev("/dev/cu.usbmodem1451")
+dev = CantactDev("/dev/cu.usbmodem1411")
 dev.set_bitrate(500000)
 dev.start()
 while True:
