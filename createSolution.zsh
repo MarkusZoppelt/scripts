7f8f3bceb5b0d11e84f912c101e35368f4ad5f68
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2018-06-29 11:41:32 +0200

    added vs vanilla solution template

--- createSolution.zsh
+++ createSolution.zsh
@@ -9,8 +9,8 @@ mkdir $projectName/$projectName
 cd $projectName
 
 cp ../$filename $projectName/.
-cp ~/code/Vanilla-sln/Vanilla.sln $projectName.sln
-cp ~/code/Vanilla-sln/Vanilla.vcxproj $projectName/$projectName.vcxproj
+cp ~/code/scripts/Vanilla-sln/Vanilla.sln $projectName.sln
+cp ~/code/scripts/Vanilla-sln/Vanilla.vcxproj $projectName/$projectName.vcxproj
 
 rm -rf .vs* .DS_Store
 sed -i -- "s/Vanilla/$projectName/g" **/*(D.)
