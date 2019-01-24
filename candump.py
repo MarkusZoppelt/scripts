a9ee4476f4dde8f5a16dccc2390325e34300051d
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2019-01-24 15:41:58 +0100

    removed a script

--- candump.py
+++
@@ -1,9 +0,0 @@
-#! /usr/local/bin/python3
-from pyvit import can
-from pyvit.hw.cantact import CantactDev
-
-dev = CantactDev("/dev/cu.usbmodem1411")
-dev.set_bitrate(500000)
-dev.start()
-while True:
-      print(dev.recv())
