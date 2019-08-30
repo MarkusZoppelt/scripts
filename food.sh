610522df3dbd0c9dd26b517020677e5649ee6c6f
Markus Zoppelt <markus.zoppelt@th-nuernberg.de>
2019-08-30 11:04:52 +0200

    cleanup

--- food.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl --silent "http://www.werkswelt.de/?id=hohf" | grep Speiseplan > food.html
-textutil -convert txt food.html
-cat food.txt
-rm food.html food.txt
--- git-eradicate.sh
+++
@@ -1,7 +0,0 @@
-git filter-branch -f  --index-filter \
-'git rm -r --force --cached --ignore-unmatch Pods' \
-    -- --all
-rm -Rf .git/refs/original && \
-git reflog expire --expire=now --all && \
-git gc --aggressive && \
-git prune
